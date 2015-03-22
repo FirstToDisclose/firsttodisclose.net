1ad28fbb321eb697e14b27451c6fd2deda2a7102
Chris C Cerami <chrisccerami@gmail.com>
2015-03-22 11:43:47 -0400

    Replace sqlite with postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150314224641) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "disclosures", force: :cascade do |t|
     t.string   "title"
     t.text     "abstract"
