2bfa929a0f5df4c0b66277a7092bd10069147838
Chris C Cerami <chrisccerami@gmail.com>
2015-03-23 01:06:16 -0400

    Add test for disclosures index page, ensuring links to each disclosure page

--- spec/features/disclosure_spec.rb
+++ spec/features/disclosure_spec.rb
@@ -35,7 +35,19 @@ describe "Disclosures" do
       click_on "Create Disclosure"
 
       expect(page).to have_content "Disclosure was successfully created"
-      expect(page).to have_content tag.name
+      expect(page).to have_link tag.name
+    end
+  end
+
+  feature "index page" do
+    it "has links to each disclosure" do
+      disclosure_1 = FactoryGirl.create(:disclosure)
+      disclosure_2 = FactoryGirl.create(:disclosure)
+
+      visit disclosures_path
+
+      expect(page).to have_link disclosure_1.title
+      expect(page).to have_link disclosure_2.title
     end
   end
 end
