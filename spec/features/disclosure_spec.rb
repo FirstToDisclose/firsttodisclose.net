e5b02f0daff65a4842fd9324afa5b84bf18a8198
Chris C Cerami <chrisccerami@gmail.com>
2015-03-22 23:18:58 -0400

    Add tags to disclosure show page

--- spec/features/disclosure_spec.rb
+++ spec/features/disclosure_spec.rb
@@ -22,5 +22,20 @@ describe "Disclosures" do
 
       expect(page).to have_content "can't be blank"
     end
+
+    it "can be tagged" do
+      tag = FactoryGirl.create(:tag)
+      visit new_disclosure_path
+
+      fill_in "Title", with: "My New Valid Disclosure"
+      fill_in "Abstract", with: "This is my disclosure" * 10
+      fill_in "Body", with: "Here is a detailed description of my disclosure" * 30
+      check "Consented"
+      check tag.name
+      click_on "Create Disclosure"
+
+      expect(page).to have_content "Disclosure was successfully created"
+      expect(page).to have_content tag.name
+    end
   end
 end
