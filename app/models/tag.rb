fc9ac931e645173c41f27f3b147150dbe60a0332
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-26 16:04:05 -0400

    Updated to innovation in models, views and controllers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
-  has_many :disclosure_tags
-  has_many :disclosures, through: :disclosure_tags
+  has_many :innovation_tags
+  has_many :innovations, through: :innovation_tags
 end
