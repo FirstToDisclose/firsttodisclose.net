567ca98d7ab95d4a5c1d8343c9d2db1557835e45
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-27 12:10:28 -0400

    Passing tests for basic textacular search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,13 @@
 class SearchController < ApplicationController
   def index
     if params[:search]
-      @results = Innovation.basic_search(params[:search])
+      search_return = Innovation.basic_search(params[:search])
+      if results = []
+        @results = nil
+        @failed_search = params[:search]
+      else
+        @results = search_return
+      end
     end
   end
 end
