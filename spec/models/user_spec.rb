dcbb74040ee80798f9d1a9aa338fe43ac01bdfa2
Jason Walsh <j@wal.sh>
2015-03-28 16:50:05 -0400

    Pending tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  pending "attributes" do
+    it { should respond_to :name }
+  end
+
+  pending "validations" do
+    it { should validate_presence_of :name }
+  end
+
+  pending "associations" do
+    it { should have_many :innovation_tags }
+    it { should have_many :innovations }
+  end
+
 end
