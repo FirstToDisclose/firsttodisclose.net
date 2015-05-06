27d9693eb8e65ceefb94fc17e754dc5cc5c3f91c
Noah Teshu <nteshu@gmail.com>
2015-05-06 13:05:35 -0400

    prevent users from accepting site-wide terms more than once

--- app/controllers/accept_terms_controller.rb
+++ app/controllers/accept_terms_controller.rb
@@ -1,8 +1,15 @@
 class AcceptTermsController < ApplicationController
+	before_filter :accepted_terms
+	def accepted_terms
+	    unless !current_user.accepted_terms
+	      redirect_to '/'
+    	end
+  	end
+  	
 	def edit
 	end
 
-	def update		
+	def update	
 		if params[:Accept]
 			Rails.logger.info "that's consent"
 			current_user.accepted_terms = true
