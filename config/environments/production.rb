b8e2a3c57e10dba739764b04a5fa32c9b413ed42
Noah Teshu <nteshu@gmail.com>
2015-06-06 12:10:03 -0400

    disable cache for action controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  #turned off cache to allow will_paginate code to work
+  config.action_controller.perform_caching = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
