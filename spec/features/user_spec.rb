840c86b213bcf4a4f29bdb498093c0c1240f1d4a
Chris C Cerami <chrisccerami@gmail.com>
2015-06-14 23:33:13 -0400

    Add basic user profile

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "Users" do
+  feature "profile page" do
+    before(:each) do
+      @user = FactoryGirl.create(:user, name: "Test User")
+      @innovation = FactoryGirl.create(:innovation, user: @user)
+    end
+
+    it "displays the user's name" do
+      visit user_path(@user)
+
+      expect(page).to have_content @user.name
+    end
+
+    it "displays the user's email if no name" do
+      nameless_user = FactoryGirl.create(:user, name: nil)
+
+      visit user_path(nameless_user)
+      expect(page).to have_content nameless_user.email
+    end
+
+    it "displays a user's innovations" do
+      visit user_path(@user)
+
+      expect(page).to have_link @innovation.title
+    end
+  end
+end
